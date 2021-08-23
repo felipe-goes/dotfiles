d61b9baa3f4cc9afa43d16cb14c8a4aa69d17379
Felipe Góes <ogoes.felipe@gmail.com>
2021-08-23 20:13:52 -0300

    Implemented check for already created session.

--- .tmux/tmuxenv.sh
+++ .tmux/tmuxenv.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+#  _
+# | |_ _ __ ___  _   ___  __
+# | __| '_ ` _ \| | | \ \/ /
+# | |_| | | | | | |_| |>  <
+#  \__|_| |_| |_|\__,_/_/\_\
+#
 
 function tmuxenv(){
   local env=$1
-  if [[ "$env" == "alura" ]]
+
+  # Check which session create
+  if [[ "${env^^}" == "ALURA" ]]
   then
     local session="Alura"
     local window="VimMD"
-    tmux new-session -d -s $session
 
-    # Vim MarkDown window
-    tmux rename-window -t 1 $window
-    tmux send-keys -t $window 'cd ~/Public/Dev/Alura' C-m 'clear' C-m
-    tmux split-window -v -p 15 -t $sessions:$window
-    tmux select-pane -t 2
-    tmux split-window -h -t $session:$window
+    local activeSession=$(tmux list-sessions | grep -i alura | sed s/:.*//)
+
+    # Check if session already exist before creating one
+    if [[ "${activeSession^^}" != "${session^^}" ]]
+    then
+      tmux new-session -d -s $session
+
+      # Vim MarkDown window
+      tmux rename-window -t 1 $window
+      tmux send-keys -t $window 'cd ~/Public/Dev/Alura' C-m 'clear' C-m
+      tmux split-window -v -p 15 -t $sessions:$window
+      tmux select-pane -t 2
+      tmux split-window -h -t $session:$window
+    fi
 
     # Attach to default window
     tmux select-pane -t 1

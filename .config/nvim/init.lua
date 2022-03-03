eb061838fbc37ccc611562b0bcf5f34dc4d3043b
Felipe Góes <ogoes.felipe@gmail.com>
2022-03-03 20:36:18 -0300

    Reorganized theme loading and a minor change to options.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,10 +3,4 @@ require "user.keymaps"
 require "user.plugins"
 require "user.airline"
 require "user.autocommands"
-
--- Apply Dracula Theme
-vim.cmd [[
-  packadd! dracula
-  syntax enable
-  colorscheme dracula
-]]
+require "user.colorscheme"

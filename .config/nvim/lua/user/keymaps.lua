cf20134470599a7f63e304e30eed23f39866e73f
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-05 18:31:32 -0300

    Added dap.lua and fixed configurations for debugging.

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -94,6 +94,7 @@ vim.cmd("let g:lion_squeeze_spaces = 1") -- Remove as many spaces as possible wh
 -- Dap Debugger
 keymap("n", "<leader>dH", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
 keymap("n", "<leader>dr", ":lua require'dap'.repl.toggle({}, 'vsplit')<CR><C-w>l", opts)
+vim.keymap.set("n", "<leader>d?", function() local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes) end)
 vim.keymap.set("n", "<C-A-k>", function() require'dap'.step_out() end)
 vim.keymap.set("n", "<C-A-l>", function() require'dap'.step_into() end)
 vim.keymap.set("n", "<C-A-j>", function() require'dap'.step_over() end)

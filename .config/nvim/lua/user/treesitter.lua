7ebd0b41d0fc161a752a96d35f14aebcde8cbb26
Felipe Góes <ogoesfelipe@gmail.com>
2024-04-23 18:05:18 -0300

    feat: create a list of languages for treesitter and delete unsed

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -4,8 +4,58 @@ if not status_ok then
   return
 end
 
+-- commented languages are not supported yet
 configs.setup({
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or list of languages
+  ensure_installed = {
+    "arduino",
+    "asm",
+    "awk",
+    "bash",
+    "c",
+    "cpp",
+    "c_sharp",
+    "cmake",
+    "css",
+    "csv",
+    "dart",
+    "diff",
+    "dockerfile",
+    "gitcommit",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitignore",
+    "go",
+    "graphql",
+    "html",
+    "http",
+    "java",
+    "javascript",
+    "json",
+    "json5",
+    "kotlin",
+    "lua",
+    "luadoc",
+    "make",
+    "markdown",
+    "matlab",
+    -- "nginx",
+    "perl",
+    "php",
+    -- "powershell",
+    "printf",
+    "python",
+    "r",
+    "regex",
+    "ruby",
+    "rust",
+    "sql",
+    -- "ssh",
+    "swift",
+    "typescript",
+    "xml",
+    "yaml"
+  }, -- one of "all", "maintained" (parsers with maintainers), or list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   autopairs = {
@@ -15,6 +65,7 @@ configs.setup({
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
+    indent = { enable = true},
   },
   indent = { enable = false, disable = { "yaml" } },
 })

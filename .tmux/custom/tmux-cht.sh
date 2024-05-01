93fac91f0f0b2c865ebf1fd60f8868ae09c1ce39
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-01 17:09:59 -0300

    fix: update to regex the string evaluation

--- .tmux/custom/tmux-cht.sh
+++ .tmux/custom/tmux-cht.sh
@@ -3,7 +3,7 @@ selected=$(cat "$HOME/.tmux/custom/languages.txt" "$HOME/.tmux/custom/core-utils
 
 existSession=$(tmux ls 2>/dev/null)
 
-if grep -qs "$selected" "$HOME/.tmux/custom/languages.txt"; then
+if grep -qs "^$selected$" "$HOME/.tmux/custom/languages.txt"; then
 	read -r -p "query: " query
 	query=$(echo "$query" | tr ' ' '+')
 	echo "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
@@ -13,7 +13,7 @@ if grep -qs "$selected" "$HOME/.tmux/custom/languages.txt"; then
 	else
 		tmux neww -n "${selected}-${query}" bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 	fi
-elif grep -qs "$selected" "$HOME/.tmux/custom/core-utils.txt"; then
+elif grep -qs "^$selected$" "$HOME/.tmux/custom/core-utils.txt"; then
 	read -r -p "query: " query
 	query=$(echo "$query" | tr ' ' '+')
 	echo "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
@@ -23,18 +23,18 @@ elif grep -qs "$selected" "$HOME/.tmux/custom/core-utils.txt"; then
 	else
 		tmux neww -n "${selected}-{$query}" bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 	fi
-elif grep -qs "$selected" "$HOME/.tmux/custom/chatgpt.txt"; then
+elif grep -qs "^$selected$" "$HOME/.tmux/custom/chatgpt.txt"; then
 	if [ "$existSession" == "" ]; then
 		bash -c "export OPENAI_API_KEY='replace-with-key' && chatgpt --set-model $(chatgpt --list-models | tail -n +2 | fzf | awk '{print $2}') && chatgpt --interactive"
 	else
 		tmux neww -n "$selected" bash -c "export OPENAI_API_KEY='replace-with-key' && chatgpt --set-model $(chatgpt --list-models | tail -n +2 | fzf | awk '{print $2}') && chatgpt --interactive"
 	fi
-elif grep -qs "$selected" "$HOME/.tmux/custom/notes.txt"; then
+elif grep -qs "^$selected$" "$HOME/.tmux/custom/notes.txt"; then
 	if [ "$existSession" == "" ]; then
 		bash -c "glow $HOME/Public/dev/utils"
 	else
 		tmux neww -n "$selected" bash -c "glow $HOME/Public/dev/notes"
 	fi
 else
-	tmux neww -n "not found" bash -c "Please select one of the listed options & while [ : ]; do sleep 1; done"
+	tmux neww -n "not-found" bash -c "echo 'Please select one of the listed options' & while [ : ]; do sleep 1; done"
 fi

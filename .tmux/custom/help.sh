9082c5490b9ce12adcd63de4ee6a5b5428265eb5
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-05 17:04:25 -0300

    refactor: update logic to use read for pausing the created tmux window

--- .tmux/custom/help.sh
+++ .tmux/custom/help.sh
@@ -9,22 +9,22 @@ if [[ "^$option$" =~ "cheat-sheet" ]]; then
 	if grep -qs "^$selected$" "$HOME/.tmux/custom/languages.txt"; then
 		read -r -p "query: " query
 		query=$(echo "$query" | tr ' ' '+')
-		echo "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+		echo "curl cht.sh/$selected/$query ; read"
 
 		if [ "$existSession" == "" ]; then
-			bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+			bash -c "curl cht.sh/$selected/$query ; read"
 		else
-			tmux neww -n "${selected}-{$query}" bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+			tmux neww -n "${selected}-{$query}" bash -c "curl cht.sh/$selected/$query ; read"
 		fi
 	elif grep -qs "^$selected$" "$HOME/.tmux/custom/core-utils.txt"; then
 		read -r -p "query: " query
 		query=$(echo "$query" | tr ' ' '+')
-		echo "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+		echo "curl cht.sh/$selected~$query ; read"
 
 		if [ "$existSession" == "" ]; then
-			bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+			bash -c "curl cht.sh/$selected~$query ; read"
 		else
-			tmux neww -n "${selected}-{$query}" bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+			tmux neww -n "${selected}-{$query}" bash -c "curl cht.sh/$selected~$query ; read"
 		fi
 	fi
 elif [[ "^$option$" =~ "chatgpt" ]]; then
@@ -43,14 +43,14 @@ elif [[ "^$option$" =~ "man" ]]; then
 	if [ "$existSession" == "" ]; then
 		bat "$HOME/.tmux/custom/languages.txt" "$HOME/.tmux/custom/core-utils.txt" | gum filter
 	else
-		tmux neww -n "$option" bash -c "man $(bat "$HOME"/.tmux/custom/languages.txt "$HOME"/.tmux/custom/core-utils.txt | gum filter) & while [ : ]; do sleep 1; done"
+		tmux neww -n "$option" bash -c "man $(bat "$HOME"/.tmux/custom/languages.txt "$HOME"/.tmux/custom/core-utils.txt | gum filter) ; read"
 	fi
 elif [[ "^$option$" =~ "eg" ]]; then
 	if [ "$existSession" == "" ]; then
 		bat "$HOME/.tmux/custom/languages.txt" "$HOME/.tmux/custom/core-utils.txt" | gum filter
 	else
-		tmux neww -n "$option" bash -c "tldr $(bat "$HOME"/.tmux/custom/languages.txt "$HOME"/.tmux/custom/core-utils.txt | gum filter) & while [ : ]; do sleep 1; done"
+		tmux neww -n "$option" bash -c "tldr $(bat "$HOME"/.tmux/custom/languages.txt "$HOME"/.tmux/custom/core-utils.txt | gum filter) ; read"
 	fi
 else
-	tmux neww -n "not-found" bash -c "echo 'Please select one of the listed options' & while [ : ]; do sleep 1; done"
+	tmux neww -n "not-found" bash -c "echo 'Please select one of the listed options' ; read"
 fi

4517912f40f8b5915dc024e33aa018579281b788
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-07 16:42:58 -0300

    feat: update visualization of csv files and limit stack overflow tags to 1000

--- .tmux/custom/stackapi/stackexchange.sh
+++ .tmux/custom/stackapi/stackexchange.sh
@@ -7,34 +7,30 @@ query=$(gum input --placeholder "query")
 case "$site" in
 "stackoverflow")
 	tag=$(
-		bat "$HOME"/.tmux/custom/stackapi/stackoverflow-tags.csv |
-			gum filter --indicator=">" |
-			cut -d ',' -f 1 |
-			sed 's/[ \t]*$//'
+		column -s, -t < "$HOME"/.tmux/custom/stackapi/stackoverflow-tags.csv |
+			gum filter --indicator=">" --no-sort |
+			cut -d ' ' -f 1
 	)
 	;;
 "superuser")
 	tag=$(
-		bat "$HOME"/.tmux/custom/stackapi/superuser-tags.csv |
-			gum filter --indicator=">" |
-			cut -d ',' -f 1 |
-			sed 's/[ \t]*$//'
+		column -s, -t < "$HOME"/.tmux/custom/stackapi/superuser-tags.csv |
+			gum filter --indicator=">" --no-sort |
+			cut -d ' ' -f 1
 	)
 	;;
 "askubuntu")
 	tag=$(
-		bat "$HOME"/.tmux/custom/stackapi/askubuntu-tags.csv |
-			gum filter --indicator=">" |
-			cut -d ',' -f 1 |
-			sed 's/[ \t]*$//'
+		column -s, -t < "$HOME"/.tmux/custom/stackapi/askubuntu-tags.csv |
+			gum filter --indicator=">" --no-sort |
+			cut -d ' ' -f 1
 	)
 	;;
 "serverfault")
 	tag=$(
-		bat "$HOME"/.tmux/custom/stackapi/serverfault-tags.csv |
-			gum filter --indicator=">" |
-			cut -d ',' -f 1 |
-			sed 's/[ \t]*$//'
+		column -s, -t < "$HOME"/.tmux/custom/stackapi/serverfault-tags.csv |
+			gum filter --indicator=">" --no-sort |
+			cut -d ' ' -f 1
 	)
 	;;
 *)

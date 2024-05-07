16beec652b770b2f0ee87be4d9ba1ff4df3b28f6
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-06 22:31:11 -0300

    refactor: update tmux main session

--- .tmux/layouts/main.session.sh
+++ .tmux/layouts/main.session.sh
@@ -5,26 +5,7 @@ session_root "$HOME"
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "main"; then
-
-  # Create a new window inline within session layout definition.
   new_window "main"
-
-  # Split vertically and horizontally
-  # split_v 20
-  # split_h 50
-
-  # Run command
-  # run_cmd "nvim .bashrc"
-
-  # Select pane
-  # select_pane 0
-
-  # Load a defined window layout.
-  #load_window "example"
-
-  # Select the default active window on session creation.
-  #select_window 1
-
 fi
 
 # Finalize session creation and switch/attach to it.

7d43fd95ddca293fb1b27529fe62bb90839fcfbf
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-07 15:07:47 -0300

    feat: hide links from stack exchange's api result

--- .tmux/custom/stackapi/filter.sh
+++ .tmux/custom/stackapi/filter.sh
@@ -3,7 +3,7 @@
 # - show querystack output using glow
 # - pick a result using gum
 # - extract web link
-glow -s dark -w 1000 |
+glow -s "$HOME"/.config/glow/stackexchange_theme.json -w 1000 |
 	gum choose --height=50 |
 	sed 's/[ \t]*$//' |
 	rev |

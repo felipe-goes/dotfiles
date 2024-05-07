e98a194ac34136f2d8c8fbface64a288f610a729
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-07 15:15:00 -0300

    feat: update tag list and file structure
    - move stack exchange's api related files to the correct directory
    - update tags list according to requested site

--- .tmux/custom/stackapi/stackexchange.sh
+++ .tmux/custom/stackapi/stackexchange.sh
@@ -3,6 +3,43 @@
 # interface for using querystack
 site=$(bat "$HOME"/.tmux/custom/stackapi/sites.txt | gum filter --indicator=">")
 query=$(gum input --placeholder "query")
-tag=$(bat "$HOME"/.tmux/custom/languages.txt "$HOME"/.tmux/custom/core-utils.txt | gum filter)
+
+case "$site" in
+"stackoverflow")
+	tag=$(
+		bat "$HOME"/.tmux/custom/stackapi/stackoverflow-tags.csv |
+			gum filter --indicator=">" |
+			cut -d ',' -f 1 |
+			sed 's/[ \t]*$//'
+	)
+	;;
+"superuser")
+	tag=$(
+		bat "$HOME"/.tmux/custom/stackapi/superuser-tags.csv |
+			gum filter --indicator=">" |
+			cut -d ',' -f 1 |
+			sed 's/[ \t]*$//'
+	)
+	;;
+"askubuntu")
+	tag=$(
+		bat "$HOME"/.tmux/custom/stackapi/askubuntu-tags.csv |
+			gum filter --indicator=">" |
+			cut -d ',' -f 1 |
+			sed 's/[ \t]*$//'
+	)
+	;;
+"serverfault")
+	tag=$(
+		bat "$HOME"/.tmux/custom/stackapi/serverfault-tags.csv |
+			gum filter --indicator=">" |
+			cut -d ',' -f 1 |
+			sed 's/[ \t]*$//'
+	)
+	;;
+*)
+	echo "Please choose one of the valid sites."
+	;;
+esac
 
 querystack "$site" "$query" "$tag"

a6e2641778e89e41cc4934910d3007b89337be8c
Felipe Góes <ogoesfelipe@gmail.com>
2024-05-11 12:53:38 -0300

    feat: add context plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -57,6 +57,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   })
+  use("nvim-treesitter/nvim-treesitter-context")
   use("nvim-tree/nvim-web-devicons")
   use("stevearc/dressing.nvim")
   use({

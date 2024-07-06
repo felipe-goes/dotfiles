8a21cf52e3c146b08446224d7f874788eb633ae0
Felipe Goes <ogoesfelipe@gmail.com>
2024-07-05 22:04:34 -0300

    fix(neovim): prioritize mason over nix

--- .install-packages/neovim.nix
+++ .install-packages/neovim.nix
@@ -1,15 +1,16 @@
 with import <nixpkgs> {}; [
 	neovim
-	pyright
-	shfmt
-	shellcheck
-	stylua
-	clang
 	ncdu
-	zk
-	lldb
 	mercurial
 	luarocks
-	julia
-	delve
+# Install with mason
+	# pyright
+	# shfmt
+	# shellcheck
+	# stylua
+	# clang
+	# zk
+	# lldb
+	# julia
+	# delve
 ]

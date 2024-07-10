7e683e80cec2482476638f04929a53328e02b9ea
Felipe Goes <ogoesfelipe@gmail.com>
2024-07-10 00:14:04 -0300

    feat(neovim): update icons

--- .config/nvim/lua/user/completions.lua
+++ .config/nvim/lua/user/completions.lua
@@ -125,11 +125,11 @@ cmp.setup({
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[NVIM_LUA]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
+        nvim_lsp = "[󰲋]",
+        nvim_lua = "[󰢱]",
+        luasnip = "[󰩫]",
+        buffer = "[󰭹]",
+        path = "[󰉋]",
       })[entry.source.name]
       return vim_item
     end,

5501f5cb31dc49fbe03e9f3c7fafcb48026f93d0
Felipe Goes <ogoesfelipe@gmail.com>
2024-11-30 13:49:52 -0300

    fix(system): update apt packages

--- .install-packages/apt.sh
+++ .install-packages/apt.sh
@@ -4,7 +4,6 @@ set -e
 
 apt update
 apt upgrade
-apt install fd-find
-apt install python3.12-venv btop yt-dlp
+apt install gcc fd-find
+apt install playerctl when taskwarrior btop yt-dlp
 apt install pandoc texlive-latex-base texlive-fonts-recommended texlive-fonts-extra
-apt dotnet-sdk-8.0 wmctrl xclip

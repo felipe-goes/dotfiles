e12acb31a8c64bba05bc0581658bfeb5f0f113e4
Felipe Goes <ogoesfelipe@gmail.com>
2024-12-27 18:42:05 -0300

    feat(system): add more packages to apt list

--- .install-packages/apt.sh
+++ .install-packages/apt.sh
@@ -4,7 +4,8 @@ set -e
 
 apt update
 apt upgrade
-apt install gcc fd-find
 apt install imagemagick
+apt install gcc fd-find
+apt install luarocks cargo composer php pip
 apt install playerctl when taskwarrior btop yt-dlp
 apt install pandoc texlive-latex-base texlive-fonts-recommended texlive-fonts-extra

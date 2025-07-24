0976f78adcdb38d06652c67bcb7c6c16d45f302e
Felipe Goes <ogoesfelipe@gmail.com>
2025-07-23 22:30:13 -0300

    style(neovim): add comments and remove unused autocommands configuration

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,42 +1,3 @@
-vim.cmd([[
-  augroup FileTypeOverrides
-    autocmd!
-    autocmd TermOpen * setlocal nospell
-  augroup END
-
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-    autocmd FileType c,cpp setlocal commentstring=//\ %s
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-    autocmd TermOpen * setlocal nospell
-  augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd TermOpen * setlocal nospell
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd TermOpen * setlocal nospell
-  augroup end
-
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-]])
-
 -- Intelligently reopens files at your last edit position
 local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
 vim.api.nvim_clear_autocmds({ group = lastplace })
@@ -62,6 +23,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
+-- Set command for Neovim `make`
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c", "cpp" },
   callback = function()
@@ -69,6 +31,7 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Set project based configuration for running the executable
 vim.api.nvim_create_user_command("Run", function()
   local project_executable = vim.g.project_executable or "SfmlGame"
   local project_build_path = vim.g.project_build_path or "./build/release/"
@@ -76,7 +39,7 @@ vim.api.nvim_create_user_command("Run", function()
   vim.cmd("split | terminal " .. exec)
 end, {})
 
--- Optional: Re-apply config when command line mode is entered
+-- Re-apply diagnostics config when command line mode is entered
 vim.api.nvim_create_autocmd({ "CmdlineEnter", "InsertLeave", "BufEnter", "ModeChanged" }, {
   callback = function()
     vim.diagnostic.config({
@@ -90,15 +53,6 @@ vim.api.nvim_create_autocmd({ "CmdlineEnter", "InsertLeave", "BufEnter", "ModeCh
         },
       },
     })
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
-    })
   end,
   desc = "Ensure virtual_text stays off",
 })
-
--- Autoformat
--- augroup _lsp
---   autocmd!
---   autocmd BufWritePre * lua vim.lsp.buf.formatting()
--- augroup end
